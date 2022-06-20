4a542f44700f0bf126672b643066e0145d69cef7
petarLecic <petar.lecic94@gmail.com>
2022-06-20 20:40:09 +0200

    Uradjena provera podrzanosti mreze (conditional rendering), dohvatanje poslednjih 5 postova, ubacena oba ABI-a.

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useWeb3React} from '@web3-react/core'
+import { ethers, BigNumber } from 'ethers'
+// import ropstenABI from '../abis/ropstenABI.json'
+import rinkebyABI from '../abis/rinkebyABI.json'
+import {useEffect} from 'react';
 import blueLogo from '../assets/images/logo-blue.svg'
 
 function Timeline() {
-    const wallet = useWeb3React();
+    const { library, chainId } = useWeb3React();
+    const [isNetworkUnsupported, setIsNetworkUnsupported] = useState(true)
+    const [posts, setPosts] = useState([])
+    
+    useEffect(() => {
+        if (chainId === 3 || chainId === 4 || chainId === undefined) setIsNetworkUnsupported(false)
+    }, [chainId])
+
+    useEffect(() => {
+        if (isNetworkUnsupported) return
+        async function fetchPosts() {
+            let provider = library || new ethers.providers.JsonRpcProvider('https://rinkeby.infura.io/v3/ff087ba9578e4e0995028c853425a591')
+            const contract = new ethers.Contract('0x9120b19e921fAf41d315B528dE711f99cf530725', rinkebyABI, provider)
+            const latestPostID =  await contract.getLatestPostID()
+            const num = latestPostID.sub(4)
+            const posts = await contract.fetchPostsRanged(num, 5)
+            setPosts(posts)
+            console.log(posts)
+        }
+        fetchPosts()
+    }, [library, isNetworkUnsupported])
 
     return (
         <div className="timeline-wrapper">
@@ -14,13 +38,16 @@ function Timeline() {
                         <button className="btn btn-blue btn-big">Write A Post</button>
                     </div>
                 </aside>
-                <div className="network-wrapper">
-                    <div className="network-inner-wrapper">
-                        <h1 className="network-title">This app supports ETH. You are currently connected to an unsupported network.</h1>
-                        <button className="btn btn-blue btn-big">Switch to Ethereum</button>
-                        <h2 className="network-subtitle">You may need to manually switch network via your wallet.</h2>
+                {
+                    isNetworkUnsupported && 
+                    <div className="network-wrapper">
+                        <div className="network-inner-wrapper">
+                            <h1 className="network-title">This app supports ETH. You are currently connected to an unsupported network.</h1>
+                            <button className="btn btn-blue btn-big">Switch to Ethereum</button>
+                            <h2 className="network-subtitle">You may need to manually switch network via your wallet.</h2>
+                        </div>
                     </div>
-                </div>
+                }
                 <aside className="timeline-right">
                     <div className="timeline-right-inner">
                         <button className="btn btn-blue btn-big">3327.eth</button>
